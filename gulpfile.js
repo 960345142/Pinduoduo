5ff6a88f2f21e0023330f6e18995cec438affbcb
guo tao <960345142@qq.com>
2019-10-11 19:49:59 +0800

    第二版

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const gulp=require("gulp");
+const sass=require("gulp-sass");
 
 
 //监听任务
@@ -7,5 +8,19 @@ gulp.task("watchall",async()=>{
 	gulp.watch("*.html",async()=>{
 		gulp.src("*.html")
 		.pipe(gulp.dest("C:\\phpStudy\\WWW\\Pinduoduo"))
+	});
+
+	//监听sass文件
+	gulp.watch("sass/**/*",async()=>{
+		gulp.src("sass/**/*")
+		.pipe(sass())
+		.pipe(gulp.dest("C:\\phpStudy\\WWW\\Pinduoduo\\css"))
 	})
 })
+
+// var gulp = require("gulp");
+
+// gulp.task("copy-html", async function(){
+// 	gulp.src("./**/**")
+// 	.pipe(gulp.dest("C:\\phpStudy\\WWW\\Pinduoduo"));
+// });
